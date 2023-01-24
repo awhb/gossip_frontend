68faab9104a1c0e609bc426b465f64e96496362e
Hang Bin Aw <awhangbin@gmail.com>
2023-01-24 21:04:06 +0800

    added more packages

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { useAppDispatch, useAppSelector } from "../hooks/redux-hooks";
 import { fetchPosts } from "../store/post-actions";
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
