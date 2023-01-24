b90ba5b6f580c93e1c28298fe1242570813e69c5
Hang Bin Aw <awhangbin@gmail.com>
2023-01-25 04:21:06 +0800

    updated login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -34,88 +34,40 @@ const Login: React.FC = () => {
   };
 
   return localStorage.hasOwnProperty("token") ? <Navigate to="/" /> : (
-    <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh">
-      <Paper elevation={3} style={{ padding: '20px' }}>
-        <form className={classes.form} onSubmit={handleSubmit}>
-          <h2>Login</h2>
-          <TextField
-            id="username"
-            label="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <br />
-          <TextField
-            id="password"
-            label="Password"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <br />
-          {isLoading && (<LoadingSpinner />)}
-          {error && (
-            <Typography variant="body2" color="error">
-              {error}
-            </Typography>
-          )}
-          <Button variant="contained" color="primary" type="submit" className={classes.button}>
-            Login
-          </Button>
-        </form>
-      </Paper>
-    </Box>
+    <div>
+      <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh">
+        <Paper elevation={3} style={{ padding: '20px' }}>
+          <form className={classes.form} onSubmit={handleSubmit}>
+            <h2>Login</h2>
+            <TextField
+              id="username"
+              label="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <br />
+            <TextField
+              id="password"
+              label="Password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <br />
+            {isLoading && (<LoadingSpinner />)}
+            {error && (
+              <Typography variant="body2" color="error">
+                {error}
+              </Typography>
+            )}
+            <Button variant="contained" color="primary" type="submit" className={classes.button}>
+              Login
+            </Button>
+          </form>
+        </Paper>
+      </Box>
+    </div>
   );
 };
 
 export default Login;
-
-// import { useState } from 'react';
-// import { useNavigate } from 'react-router-dom';
-// import { TextField, Button } from '@material-ui/core';
-// import { useDispatch } from 'react-redux';
-// import { login } from '../store/authSlice';
-// import { useTypedSelector } from '../store';
-
-// const Login: React.FC = () => {
-// const [username, setUsername] = useState('');
-// const [password, setPassword] = useState('');
-// const auth = useTypedSelector(state => state.auth);
-// const navigate = useNavigate();
-// const dispatch = useDispatch();
-
-// async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-//     e.preventDefault();
-//     await dispatch(login({username, password}));
-//     if (!auth.error) {
-//         navigate('/');
-//     }
-// }
-
-// return (
-//     <div>
-//         <form onSubmit={handleSubmit}>
-//             <TextField
-//                 id="username"
-//                 label="Username"
-//                 value={username}
-//                 onChange={(e) => setUsername(e.target.value)}
-//             />
-//             <br />
-//             <TextField
-//                 id="password"
-//                 label="Password"
-//                 type="password"
-//                 value={password}
-//                 onChange={(e) => setPassword(e.target.value)}
-//             />
-//             <br />
-//             <Button variant="contained" color="primary" type="submit">
-//                 Login
-//             </Button>
-//         </form>
-//         {auth.error && (
-//             <p>{auth.error}</p>
-//         )}
-//     </div>
-// );
